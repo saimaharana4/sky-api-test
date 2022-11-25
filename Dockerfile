c1811dbf3f1696939a6c98ea530d0c0e9e60898e
SAI PRASANNA MAHARANA <60200140+saimaharana4@users.noreply.github.com>
2022-11-25 17:11:16 +0530

    Sky API Test using Flask

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM python:3.9-alpine3.13
+LABEL maintainer="SAI"
+
+ENV PYTHONBUFFERED 1
+
+COPY ./req.txt /tmp/req.txt
+COPY ./app /app
+WORKDIR /app
+EXPOSE 5000
+
+ARG DEV=true
+RUN python -m venv /py && \
+    /py/bin/pip install --upgrade pip && \
+    /py/bin/pip install -r /tmp/req.txt && \
+    rm -rf /tmp && \
+    adduser \
+        --disabled-password \
+        --no-create-home \
+        flask-user
+
+ENV PATH="/py/bin:$PATH"
+
+USER flask-user
---
+++ app/app.py
@@ -0,0 +1,79 @@
+from flask import Flask, jsonify, request
+import requests
+import pytest
+
+app = Flask(__name__)
+
+@app.route('/markets/summaries', methods=['GET'])
+def get_market_summaries():
+    """
+    This function will get all market summaries from the Bittrex API
+    and return them in JSON format
+    """
+
+    # make the GET request to the Bittrex API
+    response = requests.get('https://api.bittrex.com/v3/markets/summaries')
+
+    # if the response is successful, convert it to JSON and return it
+    if response.status_code == 200:
+        return jsonify(response.json())
+
+@app.route('/markets/<marketSymbol>/summary', methods=['GET'])
+def get_market_summary(marketSymbol):
+    """
+    This function will get a market summary for a specific market from the
+    Bittrex API and return it in JSON format
+    """
+
+    # make the GET request to the Bittrex API
+    response = requests.get(f'https://api.bittrex.com/v3/markets/{marketSymbol}/summary')
+
+    # if the response is successful, convert it to JSON and return it
+    if response.status_code == 200:
+        return jsonify(response.json())
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    client = app.test_client()
+
+    yield client
+
+def test_get_market_summaries(client):
+    """
+    This function tests the get_market_summaries function to ensure that it
+    is returning the correct data
+    """
+
+    # make the GET request and store the response
+    response = client.get('/markets/summaries')
+
+    # convert the response data to JSON
+    data = response.get_json()
+
+    # assert that the response status code is correct
+    assert response.status_code == 200
+
+    # assert that the JSON data is not empty
+    assert data != []
+
+def test_get_market_summary(client):
+    """
+    This function tests the get_market_summary function to ensure that it
+    is returning the correct data
+    """
+
+    # make the GET request and store the response
+    response = client.get('/markets/ltc-btc/summary')
+
+    # convert the response data to JSON
+    data = response.get_json()
+
+    # assert that the response status code is correct
+    assert response.status_code == 200
+
+    # assert that the JSON data is not empty
+    assert data != []
+
+if __name__=='__main__':
+    app.run(debug=True)
